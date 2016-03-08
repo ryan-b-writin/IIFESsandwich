cb52b867bc0a458d4c223c0e0168ae86c13bf5c5
Ryan B <ryan.bliton@gmail.com>
2016-03-07 21:16:29 -0600

    checkboxes now react to mouseclicks

---
+++ javascripts/DOMHandler.js
@@ -0,0 +1,71 @@
+var checkbox = document.getElementsByTagName("input");
+var outputTarget = document.getElementById("outputTarget");
+var priceTarget = document.getElementById("priceTarget");
+
+var breadBoxes = document.getElementsByClassName("bread");
+var meatBoxes = document.getElementsByClassName("meat");
+var cheeseBoxes = document.getElementsByClassName("cheese");
+var condimentBoxes = document.getElementsByClassName("condiments");
+var veggieBoxes = document.getElementsByClassName("veggies");
+
+var breadClear = document.getElementById("breadClear");
+var meatClear = document.getElementById("meatClear");
+var cheeseClear = document.getElementsByClassName("cheeseClear");
+var condimentClear = document.getElementById("condimentClear");
+var veggieClear = document.getElementById("veggieClear");
+
+
+//add event listener to checkboxes on click
+for (var i=0; i<checkbox.length;i++) {
+  checkbox[i].addEventListener("click", function(){
+
+    //when you click a "no bread/cheese/whatever" clear the other checkboxes
+    if (this.value === "nobread") {
+      for (var j=0;j<breadBoxes.length;j++)
+      breadBoxes[j].checked = false;
+    } else if (this.value === "nomeat") {
+      for (var j=0;j<meatBoxes.length;j++)
+      meatBoxes[j].checked = false;
+    } else if (this.value === "nocheese") {
+      for (var j=0;j<cheeseBoxes.length;j++)
+      cheeseBoxes[j].checked = false;
+    } else if (this.value === "nocondiments") {
+      for (var j=0;j<condimentBoxes.length;j++)
+      condimentBoxes[j].checked = false;
+    } else if (this.value === "noveggies") {
+      for (var j=0;j<veggieBoxes.length;j++)
+      veggieBoxes[j].checked = false;
+    } 
+  })
+}
+
+//when you make a selection, deselect clear options
+for (var i=0; i<breadBoxes.length; i++) {
+  breadBoxes[i].addEventListener("click", function(){
+    breadClear.checked = false;
+  })
+}
+for (var i=0; i<meatBoxes.length; i++) {
+  meatBoxes[i].addEventListener("click", function(){
+    meatClear.checked = false;
+  })
+}
+for (var i=0; i<cheeseBoxes.length; i++) {
+  cheeseBoxes[i].addEventListener("click", function(){
+    for (var j=0; j<cheeseClear.length; j++) {
+      cheeseClear[j].checked = false;
+    }
+  })
+}
+for (var i=0; i<condimentBoxes.length; i++) {
+  condimentBoxes[i].addEventListener("click", function(){
+    condimentClear.checked = false;
+  })
+}
+for (var i=0; i<veggieBoxes.length; i++) {
+  veggieBoxes[i].addEventListener("click", function(){
+    veggieClear.checked = false;
+  })
+}
+
+
---
+++ javascripts/SandwichMaker.js
@@ -0,0 +1,19 @@
+var SandwichMaker = (function() {
+
+  // Private variable to store the price
+  var totalPrice = 0;
+
+  var getPrice = function() {
+    return totalPrice;
+  };
+
+  // Return the public interface that other code can interact with
+  return {
+    addTopping: function(toppingPrice) {
+      totalPrice += toppingPrice;
+    },
+    getPrice: function() {
+      return totalPrice;
+    }
+  };
+})();
